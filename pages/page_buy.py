d0075dc5842f29ff67ae84aa36f125da5303803c
Gabriela Vilaro <gabriela.vilaro@uala.com.ar>
2020-06-24 19:42:02 -0300

    refactor pages of wait

--- pages/page_buy.py
+++ pages/page_buy.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class PageBuy:
     def __init__(self, driver):
@@ -6,16 +8,19 @@ class PageBuy:
         self.quantity_wanted = (By.ID, 'quantity_wanted')
         self.button_plus = (By.XPATH, '//*[@id="quantity_wanted_p"]/a[2]/span/i')
 
-
     def push_quantity(self, quantity):
-        self.driver.find_element(self.quantity_wanted).clear()
-        self.driver.find_element(self.quantity_wanted).send_keys(quantity)
+        quantity_push = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.quantity_wanted))
+        quantity_push.clear()
+        quantity_push.send_keys(quantity)
 
     def add_elements(self, quantity):
         #presiona el botón, cuantas veces le pase por parámetro
         for i in range(quantity):
-            self.driver.find_element(self.button_plus).click()
+            add_element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.button_plus))
+            add_element.click()
 
     def get_number_of_elements(self):
         #devuelvo el atributo value del elemento quantity wanted
-        return self.driver.find_element(self.quantity_wanted).get_attribute('value')
+        get_number_elements = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.quantity_wanted))
+        get_number = get_number_elements.get_attribute('value')
+        return get_number
--- pages/page_results.py
+++ pages/page_results.py
@@ -1,31 +1,39 @@
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 class ResultCases:
     def __init__(self, driver):
-        self.no_result = '//*[@id="center_column"]/p'
-        self.title_banner = '//*[@id="center_column"]/h1/span[1]'
-        self.colour_orange = 'color_1'
+        self.no_result = (By.XPATH, '//*[@id="center_column"]/p')
+        self.title_banner = (By.XPATH, '//*[@id="center_column"]/h1/span[1]')
+        self.colour_orange = (By.ID, 'color_1')
+        self.select_product = (By.ID, 'selectProductSort')
         self.driver = driver
-        self.select_product = 'selectProductSort'
 
     def get_text(self):
-        return self.driver.find_element_by_xpath(self.no_result).text
+        text_get = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.no_result))
+        text = text_get.text
+        return text
 
     def return_section_title(self):
-        return self.driver.find_element_by_xpath(self.title_banner).text
+        section_title = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.title_banner))
+        title = section_title.text
+        return title
 
     def select_color(self):
-        self.driver.find_element_by_id(self.colour_orange).click()
+        select_orange = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.colour_orange))
+        select_orange.click()
 
     def product_list_by_select(self, text):
-        order = Select(self.driver.find_element_by_id(self.select_product))
+        order = Select(self.driver.find_element(self.select_product))
         order.select_by_visible_text(text)
 
     def product_list_by_value(self, value):
-        order = Select(self.driver.find_element_by_id(self.select_product))
+        order = Select(self.driver.find_element(self.select_product))
         order.select_by_value(value)
 
     def product_list_by_index(self, number):
-        order = Select(self.driver.find_element_by_id(self.select_product))
+        order = Select(self.driver.find_element(self.select_product))
         order.select_by_index(number)
 
