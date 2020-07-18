e1f5e16441b473452e08b05cbc78a824f7d4b1bf
Gabriela Vilaro <gabriela.vilaro@uala.com.ar>
2020-07-18 14:52:02 -0300

    add documentation automatice with pdocs3

--- tests/tests_index.py
+++ tests/tests_index.py
@@ -1,12 +1,18 @@
+'''Tests de la page principan de la página automationpractice.com'''
+
 from selenium import webdriver
 from pages.page_index import PageIndex
 from pages.page_results import ResultCases
 from pages.page_buy import PageBuy
 import unittest
-import pytest
+
+__pdoc__ = {}
+__pdoc__["TestSearchCases"] = False
+
 
 class TestSearchCases(unittest.TestCase):
-    # Método con pre-condiciones
+    '''Método con las pre-condiciones'''
+
     def setUp(self):
         # instancio mi driver, en este caso chromedriver
         self.driver = webdriver.Chrome('../drivers/chromedriver')
@@ -18,25 +24,29 @@ class TestSearchCases(unittest.TestCase):
         self.buyPage = PageBuy(self.driver)
 
     def test_search_no_element(self):
-        '''Este tests busca un elemento inválido y verifica que la página devuelva un mensaje de error error'''
+        ''' ##Test #1 Este tests busca un elemento inválido y verifica que la página
+        devuelva un mensaje de error error'''
 
         self.indexPage.search('Manzana')
         self.assertEqual(self.resultPage.get_text(), 'No results were found for your search "Manzana"')
 
     def test_search_element_valid(self):
-        '''Esre tests busca un elemento válido y verifica que aparezca correctamente el nombre del elemento'''
+        ''' ##Test #2
+         Este tests busca un elemento válido y verifica que aparezca correctamente el nombre del elemento'''
 
         self.indexPage.search('Dress')
         self.assertTrue('DRESS' in self.resultPage.return_section_title())
 
     def test_search_element_valid_two(self):
-        '''Esre tests busca un elemento válido y verifica que aparezca correctamente el nombre del elemento'''
+        '''##Test #3 Este tests busca un
+         elemento válido y verifica que aparezca correctamente el nombre del elemento'''
 
         self.indexPage.search('T-Shirt')
         self.assertTrue('T-SHIRT' in self.resultPage.return_section_title())
 
     def test_button_orange(self):
-        '''Este tests busca un elemento, elige el color naranja, agrega 8 y verifica que el número coincida'''
+        '''##Test #4
+        Este tests busca un elemento, elige el color naranja, agrega 8 y verifica que el número coincida'''
 
         self.indexPage.search('T-Shirt')
         self.resultPage.click_color()
@@ -46,7 +56,8 @@ class TestSearchCases(unittest.TestCase):
         self.assertEqual(number_of_elements, '8')
 
     def test_list_product_selection(self):
-        '''Este tests toma diferentes valores de la lista de métodos de filtrado de una búsqueda'''
+        '''##Test #5 Este tests toma diferentes valores de la lista de métodos
+        de filtrado de una búsqueda'''
 
         self.indexPage.search('T-Shirt')
         self.resultPage.product_list_by_select('Product Name: A to Z')
@@ -54,12 +65,13 @@ class TestSearchCases(unittest.TestCase):
         self.resultPage.product_list_by_index(3)
 
     def test_check_phone_number(self):
-        '''Este tests checkea el número de teléfono de la página'''
+        '''##Test #6 Este test checkea el número de teléfono de la página'''
 
         phone_number = self.indexPage.return_phone_number_of_banner()
         self.assertEqual(phone_number, '0123-456-789')
 
-    # Método con las post-condiciones
+    ''' Método con las post-condiciones'''
+
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
